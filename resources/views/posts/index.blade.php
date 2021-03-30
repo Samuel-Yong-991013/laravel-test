8495a67c4818ac7cf1575df0b682a91713e40648
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-03-30 17:22:09 +0800

    lorem ipsum

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,7 +3,14 @@
 @section('content')
     <div class="flex justify-center">
         <div class="w-8/12 bg-white p-6 rounded-lg">
-            Post
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Vitae semper quis lectus nulla at volutpat diam ut venenatis. Id eu nisl nunc mi ipsum faucibus vitae aliquet nec. 
+        Euismod in pellentesque massa placerat. Vitae auctor eu augue ut lectus arcu. 
+        A lacus vestibulum sed arcu non odio euismod lacinia at. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. 
+        Consectetur a erat nam at lectus urna duis. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. 
+        Ut tortor pretium viverra suspendisse potenti nullam. Amet porttitor eget dolor morbi non arcu. 
+        Rutrum quisque non tellus orci ac auctor augue mauris. Quam nulla porttitor massa id neque aliquam. 
+        Arcu cursus euismod quis viverra nibh cras pulvinar.
         </div>
     </div>
 @endsection
