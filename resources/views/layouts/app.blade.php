27545d221e90156625a8da4402e87507780906c8
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-03-30 16:49:47 +0800

    added some words in there

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,37 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
+    <body class="bg-gray-100">
+        <nav class="p-6  bg-white flex justify-between">
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">Home</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Dashboard</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Post</a>
+                </li>
+            </ul>
+
+            <ul class="flex items-center">
+                <li>
+                    <a href="" class="p-3">Samuel</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Register</a>
+                </li>
+            </ul>
+        </nav>
        @yield('content')
     </body>
 </html>
