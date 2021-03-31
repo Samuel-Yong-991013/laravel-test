ca2764eeac1d3b08ced36697c58e748ca60a44ba
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-03-31 14:41:40 +0800

    added new route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/register');
+Route::get('/register', RegisterController::class, 'index');
 
 Route::get('/', function () {
     return view('posts.index');
