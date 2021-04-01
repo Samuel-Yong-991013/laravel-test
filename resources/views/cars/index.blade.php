33d134bfca91200ab63adf5fb44365b77eb42b48
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-04-01 18:28:06 +0800

    filled index page with lorem ipsum

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,1 +1,26 @@
 @extends('layouts.app')
+
+@section('content')
+    <div class="m-auto w-4/5 py-24">
+        <div class="text-center">
+            <h1 class="text-5xl uppercase bold">
+                Cars
+            </h1>
+        </div>
+
+        <div class="w-5/6 py-10">
+            <div class="m-auto">
+                <span class="uppercase text-blue-500 font-bold text-xs italic">
+                    Founded: 2020
+                </span>
+                <h2 class="text-gray-700 text-5xl py-6">
+                    Audi
+                </h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus accusamus laudantium, magnam suscipit praesentium cum corrupti placeat voluptates nam molestiae sapiente dolores hic? Explicabo eius sint consequatur incidunt, dolore est!
+                </p>
+                <hr class="mt-4 mb-8">
+            </div>
+        </div>
+    </div>
+@endsection
