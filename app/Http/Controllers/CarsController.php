eba954fc5fa605abf6a37acae1e12d84a0e6c8e9
Samuel Yong Syn Wei <samuelyong91@gmail.com>
2021-04-06 10:16:07 +0800

    added create, edit and delete functions

--- app/Http/Controllers/CarsController.php
+++ app/Http/Controllers/CarsController.php
@@ -86,7 +86,9 @@ class CarsController extends Controller
      */
     public function edit($id)
     {
-        return view('cars.create');
+        $car = Car::find($id)->first();
+
+        return view('cars.edit')->with('car', $car);
     }
 
     /**
@@ -98,7 +100,13 @@ class CarsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $car = Car::where('id', $id)->update([
+                'name' => $request->input('name'), 
+                'founded' => $request->input('founded'), 
+                'description' => $request->input('description')
+        ]);
+        
+        return redirect('/cars');
     }
 
     /**
@@ -107,8 +115,10 @@ class CarsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Car $car)
     {
-        //
+        $car->delete();
+
+        return redirect('/cars');
     }
 }
